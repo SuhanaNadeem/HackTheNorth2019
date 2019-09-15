6f12a1267c5f4feae2b6b084b8237904e9b18017
Tommy Huang <tommyhuang999@gmail.com>
2019-09-14 20:01:12 -0400

    stuff happened. Added button

---
+++ Best AR Project/Assets/PaintManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.Experimental.XR;
+
+public class PaintManager : MonoBehaviour
+{
+    private bool isSpraying = false;
+
+    public GameObject paint;
+    public GameObject paintCanvas;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("I STARTED");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isSpraying)
+        {
+            Debug.Log("pfffff");
+            // make paint
+
+
+            Vector3 location = getLocation();
+            //Instantiate<GameObject>(paint, paintCanvas.transform);
+            makePaint();
+        }
+
+    }
+
+    
+
+
+    public void setIsSpraying(bool yeanay)
+    {
+        isSpraying = yeanay;
+    }
+
+}
